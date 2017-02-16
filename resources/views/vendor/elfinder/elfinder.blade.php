7ff7c68e358e14a49bdce73c090340b7f3d3106a
Nakamura Agatha <nakamura.agatha@gmail.com>
2017-02-17 06:13:08 +0700

    Procfile for Heroku

--- resources/views/vendor/elfinder/elfinder.blade.php
+++ resources/views/vendor/elfinder/elfinder.blade.php
@@ -8,8 +8,11 @@
 
     <!-- elFinder CSS (REQUIRED) -->
     <link rel="stylesheet" type="text/css" href="<?= asset($dir.'/css/elfinder.min.css') ?>">
-    <!-- <link rel="stylesheet" type="text/css" href="<?= asset($dir.'/css/theme.css') ?>"> -->
-    <link rel="stylesheet" type="text/css" href="<?= asset('vendor/backpack/elfinder/elfinder.backpack.theme.css') ?>">
+    <link rel="stylesheet" type="text/css" href="<?= asset($dir.'/css/theme.css') ?>">
+    
+
+    <link rel="stylesheet" type="text/css" href="<?= asset('vendor/backpack/windows10/css/theme.css') ?>">
+
 
     <!-- elFinder JS (REQUIRED) -->
     <script src="<?= asset($dir.'/js/elfinder.min.js') ?>"></script>
